b2f5294bdc9ce38b8ea61c29f23dfa89f74c988f
Asif Saeed <87099517+Aleena-55@users.noreply.github.com>
2023-03-23 16:01:14 +0500

    Added Cookies Services.

--- FeatureApp/Program.cs
+++ FeatureApp/Program.cs
@@ -1,4 +1,5 @@
 using AKSoftware.Localization.MultiLanguages;
+using BytexDigital.Blazor.Components.CookieConsent;
 using FeatureApp;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -9,5 +10,68 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddLanguageContainer(Assembly.GetExecutingAssembly());
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddCookieConsent(o =>
+{
+    o.Revision = 1;
+    o.PolicyUrl = "/cookie-policy";
 
+    // Call optional
+    o.UseDefaultConsentPrompt(prompt =>
+    {
+        prompt.Position = ConsentModalPosition.TopCenter;
+        prompt.Layout = ConsentModalLayout.Cloud;
+        prompt.SecondaryActionOpensSettings = false;
+        prompt.AcceptAllButtonDisplaysFirst = false;
+    });
+
+    o.Categories.Add(new CookieCategory
+    {
+        TitleText = new()
+        {
+            ["en"] = "Google Services",
+            ["de"] = "Google Dienste"
+        },
+        DescriptionText = new()
+        {
+            ["en"] = "Allows the integration and usage of Google services.",
+            ["de"] = "Erlaubt die Verwendung von Google Diensten."
+        },
+        Identifier = "google",
+        IsPreselected = true,
+
+        Services = new()
+        {
+            new CookieCategoryService
+            {
+                Identifier = "google-maps",
+                PolicyUrl = "https://policies.google.com/privacy",
+                TitleText = new()
+                {
+                    ["en"] = "Google Maps",
+                    ["de"] = "Google Maps"
+                },
+                ShowPolicyText = new()
+                {
+                    ["en"] = "Display policies",
+                    ["de"] = "Richtlinien anzeigen"
+                }
+            },
+            new CookieCategoryService
+            {
+                Identifier = "google-analytics",
+                PolicyUrl = "https://policies.google.com/privacy",
+                TitleText = new()
+                {
+                    ["en"] = "Google Analytics",
+                    ["de"] = "Google Analytics"
+                },
+                ShowPolicyText = new()
+                {
+                    ["en"] = "Display policies",
+                    ["de"] = "Richtlinien anzeigen"
+                }
+            }
+        }
+    });
+});
 await builder.Build().RunAsync();
